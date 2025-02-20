a9d3f7f01f156b624d72d5f1e800443a789c7913
Kyle <kyle.boudville@gmail.com>
2025-02-21 00:46:52 +0800

    how to debug and learnt a new react extension

--- code/06 Debugging/01-starting-project/src/components/Results.jsx
+++ code/06 Debugging/01-starting-project/src/components/Results.jsx
@@ -1,13 +1,17 @@
 import { calculateInvestmentResults, formatter } from '../util/investment.js';
 
 
+
 export default function Results({ input }) {
   const results = [];
   calculateInvestmentResults(input, results);
+
+  if (results.length === 0){
+    return <p className='Center'>Invalid duration value!</p>
+  }
+  //need to minus "-"
   const initialInvestment =
-    results[0].valueEndOfYear -
-    results[0].interest -
-    results[0].annualInvestment;
+    results[0].valueEndOfYear - results[0].interest - results[0].annualInvestment;
 
   return (
     <table id="result">
