b68fd7c295d23e1ec9ff08d03ce7b6e18b4e92d3
Kyle <kyle.boudville@gmail.com>
2025-02-28 12:12:44 +0800

    learning backend in react

--- code/15 HTTP Requests/01-starting-project/src/components/AvailablePlaces.jsx
+++ code/15 HTTP Requests/01-starting-project/src/components/AvailablePlaces.jsx
@@ -1,10 +1,37 @@
 import Places from './Places.jsx';
+import {useState, useEffect} from 'react';
+
+const places = localStorage.getItem('places');
 
 export default function AvailablePlaces({ onSelectPlace }) {
+  const [isFetching, setIsFetching] = useState(false);
+  const [availablePlaces, setAvailablePlaces] = useState([]);
+
+  useEffect(()=>{
+    setIsFetching(true);
+    async function fetchPlaces(){
+      const response = await fetch('http://localhost:3000/places');
+      const resData = await response.json();
+      setAvailablePlaces(resData.places);
+      setIsFetching(false);
+    }
+    
+      // .then((response)=>{
+      //   return response.json();
+      // })
+      // .then((resData)=>{
+      //   setAvailablePlaces(resData.places);
+      // });
+
+      fetchPlaces();
+  },[]);
+
   return (
     <Places
       title="Available Places"
-      places={[]}
+      places={availablePlaces}
+      isLoading={isFetching}
+      loadingText="Fetching places data..."
       fallbackText="No places available."
       onSelectPlace={onSelectPlace}
     />
